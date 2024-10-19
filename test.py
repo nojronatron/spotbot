e1aaf91d0cba070083efc5fdfab7ccfb268f9ebd
Jon Rumsey <nojronatron@users.noreply.github.com>
2024-10-19 21:39:42 +0000

    update tests (note: new upstream changes)

--- test.py
+++ test.py
@@ -3,17 +3,29 @@ import spotbot
 
 class TestSpotBot(unittest.TestCase):
 
-    def test_function_app_basic(self):
+    def test_create_content_pota(self):
         req_body = {"callsign":"KI7HSG", "source": "pota", "frequency": "14.074", "mode": "FT8", "wwffRef":"US-0052"}
         content = spotbot.create_content(req_body)
         expected = {'content': 'KI7HSG | [pota](https://api.pota.app/spot/comments/KI7HSG/US-0052) | freq: 14.074 | mode: FT8 | loc: US-0052', 'flags': 4}
         self.assertDictEqual(content, expected)
 
-    def test_function_app(self):
+    def test_create_content_sota(self):
         req_body = {"callsign":"KI7HSG", "source": "sotawatch", "frequency": "14.074", "mode": "FT8", "summitRef": "ABCD"}
         content = spotbot.create_content(req_body)
         expected = {'content': 'KI7HSG | [sotawatch](https://sotl.as/activators/KI7HSG) | freq: 14.074 | mode: FT8 | loc: ABCD', 'flags': 4}
         self.assertDictEqual(content, expected)
+        
+    def test_create_spot_deeplink(self):
+        self.assertTrue(False)
+        
+    def test_is_entity_recent(self):
+        self.assertTrue(False)
+        
+    def test_call_target(self):
+        self.assertTrue(False)
+        
+    def test_extract_message_id(self):
+        self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
